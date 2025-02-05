5d012cb07ad8882f4943adcc285d0a80ac8fb496
abrarhamim40 <abrarhamim4044j@gmail.com>
2025-02-05 13:49:44 +0600

    func add and utility class add

---
+++ GitPractice/GitPractice/AppUtility/App utility.swift
@@ -0,0 +1,14 @@
+//
+//  App utility.swift
+//  GitPractice
+//
+//  Created by Beyond_2 on 5/2/25.
+//
+
+import Foundation
+
+class AppUtility {
+    func printHello() {
+        print("Hello, World!")
+    }
+}
