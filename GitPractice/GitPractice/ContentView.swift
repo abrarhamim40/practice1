d2d96be940a5668d6d724330f931857c8b605a44
abrarhamim40 <abrarhamim4044j@gmail.com>
2025-02-05 13:12:13 +0600

    init commit

---
+++ GitPractice/GitPractice/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  GitPractice
+//
+//  Created by Beyond_2 on 5/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Abrar Hamim")
+                .padding()
+                .font(.largeTitle)
+                .frame(width: UIScreen.main.bounds.width, height: 100/2)
+        }
+        .padding()
+    
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitPractice/GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by Beyond_2 on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
